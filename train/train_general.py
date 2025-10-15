86ebafd9bbda26b7a60e8bee33fc7fe0fa63e4b4
BradleyStanleyClamp <bradley.stanley-clamp@new.ox.ac.uk>
2025-10-15 16:02:24 +0100

    basic testing infrastructure ready

--- train/train_general.py
+++ train/train_general.py
@@ -1,29 +1,30 @@
 """
-High level training script that is configured using config/train_general.yaml 
+High level training script that is configured using config/train_general.yaml
 """
 
 import hydra
 from omegaconf import DictConfig
-
 import wandb
-
-
 import lightning as L
 from lightning.pytorch import seed_everything
-
 import numpy as np
 import xarray as xr
-
 import logging
 import os
+import warnings
+
 
 logger = logging.getLogger(__name__)
 
 
 @hydra.main(version_base=None, config_path="../config", config_name="train_general")
 def main(cfg: DictConfig):
-    logging.info(f"test 1")
+
+    logging.info("test 1")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+
     main()
